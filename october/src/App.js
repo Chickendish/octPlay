5095b96f659a5cc7241f8b1a39833844def6c31d
Bill Donovan <williamcdonovan@gmail.com>
2017-10-26 15:06:15 -0600

    added a new component

--- october/src/App.js
+++ october/src/App.js
@@ -13,6 +13,9 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <p className="October-content">
+          This application is going to deal with the best recipes for yappy lapdogs.
+        </p>
       </div>
     );
   }
